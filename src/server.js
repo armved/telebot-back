c0d05f8296d11cee4676f66b022dce6323904574
Avetik <avetik.harutyunyan1@gmail.com>
2018-05-29 20:38:33 +0300

    Add ESLint and reformat

--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ const packageInfo = require('../package.json');
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.json({ version: packageInfo.version });
 });
 
-const server = app.listen(process.env.PORT, function () {
-  const host = server.address().address;
-  const port = server.address().port;
+const server = app.listen(process.env.PORT, () => {
+  const { port } = server.address();
 
-  console.log('Web server started at http://%s:%s', host, port);
+  console.log(`Web server started on port ${port}`);
 });
