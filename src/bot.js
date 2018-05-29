10466b3150f04abcad8e58eca00b940378d1eb48
Avetik <avetik.harutyunyan1@gmail.com>
2018-05-29 20:43:20 +0300

    Reformat code

--- src/bot.js
+++ src/bot.js
@@ -1,25 +1,25 @@
 const TelegramBot = require('node-telegram-bot-api');
-const snoowrap = require('snoowrap');
-const { TELEGRAM_TOKEN, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD } = process.env;
+const Snoowrap = require('snoowrap');
 
-const requester = new snoowrap({
+const {
+  TELEGRAM_TOKEN, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD,
+} = process.env;
+
+const requester = new Snoowrap({
   userAgent: 'telebot-back',
   clientId: REDDIT_CLIENT_ID,
   clientSecret: REDDIT_CLIENT_SECRET,
   username: REDDIT_USERNAME,
-  password: REDDIT_PASSWORD
+  password: REDDIT_PASSWORD,
 });
 
 
-const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
-bot.onText(/\/img/, (msg, match) => {
+bot.onText(/\/img/, (msg) => {
   const chatId = msg.chat.id;
 
-  requester.getSubreddit('ProgrammerHumor').getHot().then(res => {
-
+  requester.getSubreddit('ProgrammerHumor').getHot().then((res) => {
     bot.sendPhoto(chatId, res[Math.floor(Math.random() * res.length) + 1].url);
-
   });
-
 });
