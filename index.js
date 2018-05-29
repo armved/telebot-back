f6a8f427e394d6e6e6b2e0037625c384724106ae
Avetik <avetik.harutyunyan1@gmail.com>
2018-05-29 20:22:43 +0300

    Working prototype

---
+++ index.js
@@ -0,0 +1,2 @@
+require('dotenv').config();
+const bot = require('./src/bot');
---
+++ src/bot.js
@@ -0,0 +1,25 @@
+const TelegramBot = require('node-telegram-bot-api');
+const snoowrap = require('snoowrap');
+const { TELEGRAM_TOKEN, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD } = process.env;
+
+const requester = new snoowrap({
+  userAgent: 'telebot-back',
+  clientId: REDDIT_CLIENT_ID,
+  clientSecret: REDDIT_CLIENT_SECRET,
+  username: REDDIT_USERNAME,
+  password: REDDIT_PASSWORD
+});
+
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
+
+bot.onText(/\/img/, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  requester.getSubreddit('ProgrammerHumor').getHot().then(res => {
+
+    bot.sendPhoto(chatId, res[Math.floor(Math.random() * res.length) + 1].url);
+
+  });
+
+});
